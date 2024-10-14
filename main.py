4e52814a8d3b972ba90645a633456681c3c17f7f
Jaemy Ho <jaemy.ho@mail.com>
2024-10-14 17:47:41 +1300

    add decorator in main.py for login_check

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #Testing MVC Flask
-from flask import Flask, session
+from flask import Flask, session, redirect
 from flask_mysqldb import MySQL
 from config import Config
-from controllers.auth_controller import AuthController
 from controllers.expenses_controller import ExpenseController
 from controllers.database_controller import DatabaseController
 from controllers.auditlog_controller import AuditlogController
 from controllers.auth_controller import AuthController
+from functools import wraps
 
 app = Flask(__name__, template_folder='views')  # Change the template folder to 'views'
 
@@ -21,6 +21,14 @@ auditlog_controller = AuditlogController(mysql)
 auth_controller = AuthController(mysql,app)
 
 # Routes
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        redirect_response = auth_controller.check_login()
+        if redirect_response:
+            return redirect_response
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -34,44 +42,43 @@ def login():
 def logout():
     return auth_controller.logout()
 
-@app.route('/dashboard', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+@login_required
 def index():
     return expense_controller.index()
-@app.route('/add_expense', methods=['GET', 'POST'])
-def add_expense():
-    # Check if the user is logged in
-    redirect_response = auth_controller.check_login()
-    if redirect_response:
-        return redirect_response  # Redirect to login if not logged in
 
-    # Proceed to add expense if logged in
+@app.route('/add_expense', methods=['GET', 'POST'])
+@login_required
+def add_expense():
     return expense_controller.add_expense()
 
 @app.route('/edit_expense/<int:expense_id>', methods=['GET', 'POST'])
+@login_required
 def edit_expense(expense_id):
     return expense_controller.edit_expense(expense_id)
 
 @app.route('/delete_expense/<int:expense_id>', methods=['GET', 'POST'])
+@login_required
 def delete_expense(expense_id):
     return expense_controller.delete_expense(expense_id)
 
 @app.route('/expenses/scan_file', methods=['GET', 'POST'])
+@login_required
 def upload_file():
-    redirect_response = auth_controller.check_login()
-    if redirect_response:
-        return redirect_response  # Redirect to login if not logged in
-
     return expense_controller.get_receipt_data()
 
 @app.route('/expenses/upload_bank_statement', methods=['GET', 'POST'])
+@login_required
 def upload_bank_statement():
     return expense_controller.get_bank_statement_data()
 
 @app.route('/report', methods=['GET', 'POST'])
+@login_required
 def report():
     return expense_controller.view_expense()
 
 @app.route('/auditlogreport', methods=['GET', 'POST'])
+@login_required
 def auditlogreport():
     return auditlog_controller.view_auditlog()
 
