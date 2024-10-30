3b35cccfe0f3112e429d086da44fd6c01ffea5c7
Jaemy Ho <jaemy.ho@mail.com>
2024-10-31 11:46:47 +1300

    fixes

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
 class Config:
+    #Connect to Remote Server
     MYSQL_HOST = 'p3nlmysql175plsk.secureserver.net'
     MYSQL_USER = 'remote_user'
     MYSQL_PASSWORD = 'ABcd!@34'
     MYSQL_DB = 'JaemyWeb'
+
+    #Connect to Local host
+    #MYSQL_HOST = 'localhost'
+    #MYSQL_USER = 'root'
+    #MYSQL_PASSWORD = 'abc123'
+    #MYSQL_DB = 'JaemyWeb'
+
     MYSQL_CURSORCLASS = 'DictCursor'  # This ensures the cursor returns dictionaries
     MYSQL_CONNECT_TIMEOUT = 28800  # Increase the connect timeout (in seconds)
     MYSQL_WAIT_TIMEOUT = 28800  # Increase the wait timeout (in seconds)
